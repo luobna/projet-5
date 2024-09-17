d743f9413d11e4419130ed340ccf486706df5f49
Loubna Gharbi <loulizoga@gmail.com>
2024-09-17 17:37:28 +0200

    Etape10

--- src/components/Erreur404.jsx
+++ src/components/Erreur404.jsx
@@ -4,7 +4,7 @@ const Error404 = () => {
   return (
     <div className="erreur404">
       <h1>404</h1>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <p>Oups! La page que <br/>vous demandez n'existe pas.</p>
       <ul>
         <NavLink to="/">
           <li>Retourner sur la page d’accueil</li>
